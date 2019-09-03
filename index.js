3e51e0ff188ac43895537319fce4a7a484515e66
ducmanhnguyen <ducmanh93vietnam@gmail.com>
2019-09-03 18:26:42 +0200

    add destroy at end of queue stream

--- index.js
+++ index.js
@@ -1,145 +1,127 @@
 var stream = require('readable-stream')
 
-function toStreams2Obj (s) {
-  return toStreams2(s, { objectMode: true, highWaterMark: 16 })
+function toStreams2Obj(s) {
+    return toStreams2(s, {
+        objectMode: true
+        , highWaterMark: 16
+    })
 }
 
-function toStreams2Buf (s) {
-  return toStreams2(s)
+function toStreams2Buf(s) {
+    return toStreams2(s)
 }
 
-function toStreams2 (s, opts) {
-  if (!s || typeof s === 'function' || s._readableState) return s
-
-  var wrap = new stream.Readable(opts).wrap(s)
-  if (s.destroy) {
-    wrap.destroy = s.destroy.bind(s)
-  }
-  return wrap
+function toStreams2(s, opts) {
+    if (!s || typeof s === 'function' || s._readableState) return s
+    var wrap = new stream.Readable(opts).wrap(s)
+    if (s.destroy) {
+        wrap.destroy = s.destroy.bind(s)
+    }
+    return wrap
 }
-
 class MultiStream extends stream.Readable {
-  constructor (streams, opts) {
-    super(opts)
-
-    this.destroyed = false
-
-    this._drained = false
-    this._forwarding = false
-    this._current = null
-    this._toStreams2 = (opts && opts.objectMode) ? toStreams2Obj : toStreams2Buf
-
-    if (typeof streams === 'function') {
-      this._queue = streams
-    } else {
-      this._queue = streams.map(this._toStreams2)
-      this._queue.forEach(stream => {
-        if (typeof stream !== 'function') this._attachErrorListener(stream)
-      })
+    constructor(streams, opts) {
+        super(opts)
+        this.destroyed = false
+        this._drained = false
+        this._forwarding = false
+        this._current = null
+        this._toStreams2 = (opts && opts.objectMode) ? toStreams2Obj : toStreams2Buf
+        if (typeof streams === 'function') {
+            this._queue = streams
+        }
+        else {
+            this._queue = streams.map(this._toStreams2)
+            this._queue.forEach(stream => {
+                if (typeof stream !== 'function') this._attachErrorListener(stream)
+            })
+        }
+        this._next()
     }
-
-    this._next()
-  }
-
-  _read () {
-    this._drained = true
-    this._forward()
-  }
-
-  _forward () {
-    if (this._forwarding || !this._drained || !this._current) return
-    this._forwarding = true
-
-    var chunk
-    while ((chunk = this._current.read()) !== null && this._drained) {
-      this._drained = this.push(chunk)
+    _read() {
+        this._drained = true
+        this._forward()
     }
-
-    this._forwarding = false
-  }
-
-  destroy (err) {
-    if (this.destroyed) return
-    this.destroyed = true
-
-    if (this._current && this._current.destroy) this._current.destroy()
-    if (typeof this._queue !== 'function') {
-      this._queue.forEach(stream => {
-        if (stream.destroy) stream.destroy()
-      })
+    _forward() {
+        if (this._forwarding || !this._drained || !this._current) return
+        this._forwarding = true
+        var chunk
+        while ((chunk = this._current.read()) !== null && this._drained) {
+            this._drained = this.push(chunk)
+        }
+        this._forwarding = false
     }
-
-    if (err) this.emit('error', err)
-    this.emit('close')
-  }
-
-  _next () {
-    this._current = null
-
-    if (typeof this._queue === 'function') {
-      this._queue((err, stream) => {
-        if (err) return this.destroy(err)
-        stream = this._toStreams2(stream)
-        this._attachErrorListener(stream)
-        this._gotNextStream(stream)
-      })
-    } else {
-      var stream = this._queue.shift()
-      if (typeof stream === 'function') {
-        stream = this._toStreams2(stream())
-        this._attachErrorListener(stream)
-      }
-      this._gotNextStream(stream)
+    destroy(err) {
+        if (this.destroyed) return
+        this.destroyed = true
+        if (this._current && this._current.destroy) this._current.destroy()
+        if (typeof this._queue !== 'function') {
+            this._queue.forEach(stream => {
+                if (stream.destroy) stream.destroy()
+            })
+        }
+        if (err) this.emit('error', err)
+        this.emit('close')
     }
-  }
-
-  _gotNextStream (stream) {
-    if (!stream) {
-      this.push(null)
-      this.destroy()
-      return
+    _next() {
+        this._current = null
+        if (typeof this._queue === 'function') {
+            this._queue((err, stream) => {
+                if (err) return this.destroy(err)
+                stream = this._toStreams2(stream)
+                this._attachErrorListener(stream)
+                this._gotNextStream(stream)
+            })
+        }
+        else {
+            var stream = this._queue.shift()
+            if (typeof stream === 'function') {
+                stream = this._toStreams2(stream())
+                this._attachErrorListener(stream)
+            }
+            this._gotNextStream(stream)
+        }
     }
-
-    this._current = stream
-    this._forward()
-
-    const onReadable = () => {
-      this._forward()
+    _gotNextStream(stream) {
+        if (!stream) {
+            this.push(null)
+            this.destroy()
+            return
+        }
+        this._current = stream
+        this._forward()
+        const onReadable = () => {
+            this._forward()
+        }
+        const onClose = () => {
+            if (!stream._readableState.ended) {
+                this.destroy()
+            }
+        }
+        const onEnd = () => {
+            this._current.destroy();
+            this._current = null
+            stream.removeListener('readable', onReadable)
+            stream.removeListener('end', onEnd)
+            stream.removeListener('close', onClose)
+            stream.destroy();
+            this._next()
+        }
+        stream.on('readable', onReadable)
+        stream.once('end', onEnd)
+        stream.once('close', onClose)
     }
-
-    const onClose = () => {
-      if (!stream._readableState.ended) {
-        this.destroy()
-      }
+    _attachErrorListener(stream) {
+        if (!stream) return
+        const onError = (err) => {
+            stream.removeListener('error', onError)
+            this.destroy(err)
+        }
+        stream.once('error', onError)
     }
-
-    const onEnd = () => {
-      this._current = null
-      stream.removeListener('readable', onReadable)
-      stream.removeListener('end', onEnd)
-      stream.removeListener('close', onClose)
-      this._next()
-    }
-
-    stream.on('readable', onReadable)
-    stream.once('end', onEnd)
-    stream.once('close', onClose)
-  }
-
-  _attachErrorListener (stream) {
-    if (!stream) return
-
-    const onError = (err) => {
-      stream.removeListener('error', onError)
-      this.destroy(err)
-    }
-
-    stream.once('error', onError)
-  }
 }
-
-MultiStream.obj = streams => (
-  new MultiStream(streams, { objectMode: true, highWaterMark: 16 })
-)
-
+MultiStream.obj = streams => (new MultiStream(streams, {
+    objectMode: true
+    , highWaterMark: 16
+}))
 module.exports = MultiStream
